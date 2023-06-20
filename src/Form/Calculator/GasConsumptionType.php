a46ef191a5a32dd86ebb4f19be22356dc607f644
lavreek <lavreeksolacki@yandex.ru>
2023-06-20 15:24:13 +0300

    Choices added.

--- src/Form/Calculator/GasConsumptionType.php
+++ src/Form/Calculator/GasConsumptionType.php
@@ -3,6 +3,7 @@
 namespace App\Form\Calculator;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -28,6 +29,52 @@ class GasConsumptionType extends AbstractType
                     'class' => 'mb-3'
                 ]
             ])
+            ->add('gc_ro0', NumberType::class, [
+                'label' => 'Введите: ρ₀',
+                'label_attr' => [
+                    'class' => 'form-label'
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'row_attr' => [
+                    'class' => 'mb-3'
+                ]
+            ])
+            ->add('gc_ro1', ChoiceType::class, [
+                'label' => 'Введите: ρ¹',
+                'label_attr' => [
+                    'class' => 'form-label'
+                ],
+                'choices' => [
+                    'Воздух' => 1,
+                    'Аргон (Ar)' => 1.3796,
+                    'Аммиак (NH3) (сухой)' => 0.5963,
+                    'Бутан (C4H10)' => 2.088,
+                    'Углекислый газ (C02)' => 1.529,
+                    'Оксид углерода (CO)' => 0.967,
+                    'Хлор (CL2) (сухой)' => 2.486,
+                    'Этан (C2H6)' => 1.0493,
+                    'Ацетилен (C2H2)' => 0.9073,
+                    'Этилен (C2H4)' => 0.9749,
+                    'Гелий (He)' => 0.138,
+                    'Водород (H2)' => 0.0695,
+                    'Метан (CH4)' => 0.5544,
+                    'Природный газ (типовой)' => 0.65,
+                    'Азот (N2)' => 0.9672,
+                    'Оксид азота (NO)' => 1.0366,
+                    'Закись азота (N2О)' => 1.5297,
+                    'Кислород (O2)' => 1.1053,
+                    'Пропан (C3H8)' => 1.562,
+                    'Пропилен (C3H6)' => 1.452,
+                ],
+                'row_attr' => [
+                    'class' => 'mb-3'
+                ],
+                'attr' => [
+                    'class' => 'form-control form-select'
+                ]
+            ])
             ->add('gc_r0', NumberType::class, [
                 'label' => 'Введите: P₀',
                 'label_attr' => [
@@ -43,23 +90,6 @@ class GasConsumptionType extends AbstractType
             ->add('gc_r1', HiddenType::class, [
                 'attr' => [
                     'value' => '1'
-                ]
-            ])
-            ->add('gc_ro0', NumberType::class, [
-                'label' => 'Введите: ρ₀',
-                'label_attr' => [
-                    'class' => 'form-label'
-                ],
-                'attr' => [
-                    'class' => 'form-control'
-                ],
-                'row_attr' => [
-                    'class' => 'mb-3'
-                ]
-            ])
-            ->add('gc_ro1', HiddenType::class, [
-                'attr' => [
-                    'value' => '1.013'
                 ]
             ])
             ->add('gc_t0', NumberType::class, [
